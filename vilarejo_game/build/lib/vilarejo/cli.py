4f5e854a33a9c275b6a744dab74ee7909f883a98
Rackeadoexe <caduparilla2018@gmail.com>
2026-05-15 17:21:25 -0300

    Add files via upload

---
+++ vilarejo_game/build/lib/vilarejo/cli.py
@@ -0,0 +1,18 @@
+from vilarejo_game import iniciar_jogo
+
+
+def main():
+
+    while True:
+
+        iniciar_jogo()
+
+        jogar_novamente = input("Deseja jogar novamente? (s/n): ").lower()
+
+        if jogar_novamente != "s":
+            print("Obrigado por jogar!(sobreviveu mais aqui do que no jogo)")
+            break
+
+
+if __name__ == "__main__":
+    main()
